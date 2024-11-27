c07718609826968a85f6e745d1e0e7290f29dc9b
Shahman Ali <saliac@connect.ust.hk>
2024-11-27 00:48:39 -0800

    update for vercel 	modified:   app.py 	modified:   requirements.txt 	new file:   vercel.json

--- app.py
+++ app.py
@@ -5,14 +5,15 @@ app = Flask(__name__,
     template_folder='templates')
 
 @app.route('/')
-@app.route('/index.html')
 def index():
     return render_template('index.html')
 
+@app.route('/projects')
 @app.route('/projects.html')
 def projects():
     return render_template('projects.html')
 
+@app.route('/books')
 @app.route('/books.html')
 def books():
     return render_template('books.html')
@@ -27,4 +28,4 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(host='0.0.0.0', port=8080) 
